d5ae45ed10b8a01ea2f3504b248d1f51c9d397b9
MushfiqMoon <mushfiq.moon013@google.com>
2022-09-29 03:26:30 +0600

    color theme change from info to warning

--- cardio-master/src/components/Card/Card.js
+++ cardio-master/src/components/Card/Card.js
@@ -9,9 +9,9 @@ const Card = ({ data,handleCardioAdd }) => {
         <div className="card my-3">
             <img src={url} className="card-img-top" alt="cardio-thumb" />
             <div className="card-body bg-light">
-                <h3 className="card-title">{name}</h3>
-                <p className="card-text fs-5">SET: {set}<br />Time Duration: {duration}m</p>
-                <button onClick={()=>{handleCardioAdd(duration)}}  className="btn btn-info text-white fs-5">Add to List</button>
+                <h3 className="card-title text-warning">{name}</h3>
+                <p className="card-text fs-5">SET: {set}<br />Time Duration: <span className='text-warning'>{duration}m</span></p>
+                <button onClick={()=>{handleCardioAdd(duration)}}  className="btn btn-warning text-white fs-5">Add to List</button>
             </div>
         </div>
     )
--- cardio-master/src/components/Sidebar/Sidebar.js
+++ cardio-master/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Sidebar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapPin, faWeightScale, faTextHeight } from '@fortawesome/free-solid-svg-icons'
 
@@ -17,32 +18,32 @@ const Sidebar = ({ durations }) => {
 
     return (
         <>
-            <div className="d-flex align-items-center justify-content-center bg-white py-3 rounded border border-1 mb-5">
-                <img src="https://avatars.githubusercontent.com/u/28301945?s=40&amp;v=4" alt="" className="rounded border border-5"></img>
-                <div className='ps-3'>
-                    <h4 className='text-info'>Mushfiqur Rahman </h4>
-                    <FontAwesomeIcon icon={faMapPin} className="me-3" /> Dhaka, Bangladesh
+            <div className='sidebar-content'>
+                <div className="d-flex align-items-center justify-content-center bg-white py-3 rounded border border-1 mb-5">
+                    <img src="https://avatars.githubusercontent.com/u/28301945?s=40&amp;v=4" alt="" className="rounded border border-5"></img>
+                    <div className='ps-3'>
+                        <h4 className='text-warning'>Mushfiqur Rahman </h4>
+                        <FontAwesomeIcon icon={faMapPin} className="me-3" /> Dhaka, Bangladesh
+                    </div>
+                </div>
+
+                <div className="d-flex align-items-center justify-content-around bg-white py-3 rounded border border-1 mb-5">
+                    <div className='ps-3 text-center'>
+                        <h4 className='text-warning'>85 KG </h4>
+                        <FontAwesomeIcon icon={faWeightScale} className="me-1" /> Weight
+                    </div>
+                    <div className='ps-3 text-center'>
+                        <h4 className='text-warning'>5'8" </h4>
+                        <FontAwesomeIcon icon={faTextHeight} className="me-1" /> Height
+                    </div>
+                </div>
+
+                <h2 className='text-warning mb-4'>Exercise Detais</h2>
+                <div className="d-flex align-items-center justify-content-around bg-white py-3 rounded border border-1">
+                    <h5>Time Duration</h5>
+                    <h5>{totalTime} Minute</h5>
                 </div>
             </div>
-
-            <div className="d-flex align-items-center justify-content-around bg-white py-3 rounded border border-1 mb-5">
-                <div className='ps-3 text-center'>
-                    <h4 className='text-info'>85 KG </h4>
-                    <FontAwesomeIcon icon={faWeightScale} className="me-1" /> Weight
-                </div>
-                <div className='ps-3 text-center'>
-                    <h4 className='text-info'>5'8" </h4>
-                    <FontAwesomeIcon icon={faTextHeight} className="me-1" /> Height
-                </div>
-            </div>
-
-            <h2 className='text-info mb-4'>Exercise Detais</h2>
-            <div className="d-flex align-items-center justify-content-around bg-white py-3 rounded border border-1">
-                <h5>Time Duration</h5>
-                <h5>{totalTime} Minute</h5>
-            </div>
-
-
         </>
     )
 }
